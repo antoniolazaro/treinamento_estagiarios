827e7f2b6cf0ee43c81a0970d1c8f216ea0d0caf
Neyde Karen <neydekaren@infojr.com.br>
2016-03-24 00:53:37 -0300

    Tirado atributo codigo que tava gerando erro. Atributo já consta na abstracat ModelAB

--- Stags/src/com/indracompany/stags/model/ClienteModel.java
+++ Stags/src/com/indracompany/stags/model/ClienteModel.java
@@ -2,14 +2,12 @@ package com.indracompany.stags.model;
 
 public class ClienteModel extends ModelAB{
 	private String nome;
-	private String codigo;
 	private String idade;
 	private String cpf;
 
 	public ClienteModel(String nome, String codigo, String idade, String cpf) {
 		super();
 		this.nome = nome;
-		this.codigo = codigo;
 		this.idade = idade;
 		this.cpf = cpf;
 	}
@@ -20,14 +18,6 @@ public class ClienteModel extends ModelAB{
 
 	public void setNome(String nome) {
 		this.nome = nome;
-	}
-
-	public String getCodigo() {
-		return codigo;
-	}
-
-	public void setCodigo(String codigo) {
-		this.codigo = codigo;
 	}
 
 	public String getIdade() {
