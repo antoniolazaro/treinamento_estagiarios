c4fe9db754618db892d08272a9150bcd642898ee
Neyde Karen <neydekaren@infojr.com.br>
2016-03-24 00:44:06 -0300

    Adicionando o atributo nome e seus acessórios a classe midia model.

--- Stags/src/com/indracompany/stags/model/MidiaModel.java
+++ Stags/src/com/indracompany/stags/model/MidiaModel.java
@@ -1,22 +1,32 @@
 package com.indracompany.stags.model;
 
 public class MidiaModel extends ModelAB {
+	private String nome;
 	private String descricao;
 	private String codigoDeBarras;
 	private String dataDeCompra;
 	private String valorDeCompra;
 	private String valorDeAluguel;
 	private String valorDeVenda;
-	
-	public MidiaModel(String descricao, String codigoDeBarras, String dataDeCompra, String valorDeCompra,
+
+	public MidiaModel(String nome, String descricao, String codigoDeBarras, String dataDeCompra, String valorDeCompra,
 			String valorDeAluguel, String valorDeVenda) {
 		super();
+		this.nome = nome;
 		this.descricao = descricao;
 		this.codigoDeBarras = codigoDeBarras;
 		this.dataDeCompra = dataDeCompra;
 		this.valorDeCompra = valorDeCompra;
 		this.valorDeAluguel = valorDeAluguel;
 		this.valorDeVenda = valorDeVenda;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
 	}
 
 	public String getDescricao() {
@@ -66,9 +76,11 @@ public class MidiaModel extends ModelAB {
 	public void setValorDeVenda(String valorDeVenda) {
 		this.valorDeVenda = valorDeVenda;
 	}
-	
-	
-	
-	
-	
+
+	@Override
+	public String toString() {
+		return "MidiaModel [nome=" + nome + ", descricao=" + descricao + ", codigoDeBarras=" + codigoDeBarras
+				+ ", dataDeCompra=" + dataDeCompra + ", valorDeCompra=" + valorDeCompra + ", valorDeAluguel="
+				+ valorDeAluguel + ", valorDeVenda=" + valorDeVenda + "]";
+	}
 }
