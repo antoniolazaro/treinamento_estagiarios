2b9dcf1470e71606abf96aa5d8cea2d9fcd49762
Neyde Karen <neydekaren@infojr.com.br>
2016-03-24 00:38:15 -0300

    Adicionando construtor a classe MidiaModel.

--- Stags/src/com/indracompany/stags/model/MidiaModel.java
+++ Stags/src/com/indracompany/stags/model/MidiaModel.java
@@ -8,6 +8,19 @@ public class MidiaModel extends ModelAB {
 	private String valorDeAluguel;
 	private String valorDeVenda;
 	
+	public MidiaModel(String descricao, String codigoDeBarras, String dataDeCompra, String valorDeCompra,
+			String valorDeAluguel, String valorDeVenda) {
+		super();
+		this.descricao = descricao;
+		this.codigoDeBarras = codigoDeBarras;
+		this.dataDeCompra = dataDeCompra;
+		this.valorDeCompra = valorDeCompra;
+		this.valorDeAluguel = valorDeAluguel;
+		this.valorDeVenda = valorDeVenda;
+	}
+	
+	
+	
 	
 	
 }
