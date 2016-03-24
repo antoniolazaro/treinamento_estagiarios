c743159406244a8a3dd7a94bc478216e462cd0eb
Neyde Karen <neydekaren@infojr.com.br>
2016-03-24 00:46:44 -0300

    Adicionados novos atributos a classe ClienteModel

--- Stags/src/com/indracompany/stags/model/ClienteModel.java
+++ Stags/src/com/indracompany/stags/model/ClienteModel.java
@@ -2,6 +2,9 @@ package com.indracompany.stags.model;
 
 public class ClienteModel extends ModelAB{
 	private String nome;
+	private String codigo;
+	private String idade;
+	private String cpf;
 
 	public String getNome() {
 		return nome;
