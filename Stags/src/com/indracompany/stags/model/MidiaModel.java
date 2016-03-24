02dc9b20bf4d014ac61c22f30db7753fabbb71bb
Neyde Karen <neydekaren@infojr.com.br>
2016-03-24 00:36:06 -0300

    Adicionando classe Midia e seus atributos.

---
+++ Stags/src/com/indracompany/stags/model/MidiaModel.java
@@ -0,0 +1,13 @@
+package com.indracompany.stags.model;
+
+public class MidiaModel extends ModelAB {
+	private String descricao;
+	private String codigoDeBarras;
+	private String dataDeCompra;
+	private String valorDeCompra;
+	private String valorDeAluguel;
+	private String valorDeVenda;
+	
+	
+	
+}
