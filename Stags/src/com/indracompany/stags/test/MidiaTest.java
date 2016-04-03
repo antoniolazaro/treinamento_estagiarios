a04e0ac32eefd39f5c71ce02aa00e1380000d3fb
Neyde Karen <neydekaren@infojr.com.br>
2016-04-03 18:54:44 -0300

    MidiaTest rodando

--- Stags/src/com/indracompany/stags/test/MidiaTest.java
+++ Stags/src/com/indracompany/stags/test/MidiaTest.java
@@ -17,7 +17,7 @@ public class MidiaTest {
 	public void inserirMidia() throws Exception{
 		
 		MidiaModel lMidia1 = new MidiaModel(null, null, null, null, null, null, null);
-		lMidia1.setNome("Antônio Visionário");
+		lMidia1.setNome("2013");
 		lMidia1.setCodigo((long) 1);
 		lMidia1.setDescricao("25");
 		lMidia1.setCodigoDeBarras("7364736435246");
@@ -27,45 +27,12 @@ public class MidiaTest {
 		lMidia1.setValorDeVenda("lMidia1.getValorDeCompra() ou lMidia1.getValorDeAluguel()");
 		lMidia1.setTipoDeMidia(TipoDeMidiaModel.DVD);
 		lMidia1.setAtivo(true);
-		
-		MidiaModel lMidia2 = new MidiaModel(null, null, null, null, null);
-		lMidia2.setNome("Luis Cardoso");
-		lMidia2.setCodigo((long) 2);
-		lMidia2.setIdade("45");
-		lMidia2.setCpf("7364736435236");
-		lMidia2.setAtivo(true);
-		
-		MidiaModel lMidia3 = new MidiaModel(null, null, null, null, null);
-		lMidia3.setNome("Caio");
-		lMidia3.setCodigo((long) 3);
-		lMidia3.setIdade("23");
-		lMidia3.setCpf("7364736433236");
-		lMidia3.setAtivo(true);
-		
-		MidiaModel lMidia4 = new MidiaModel(null, null, null, null, null);
-		lMidia4.setNome("Karen");
-		lMidia4.setCodigo((long) 4);
-		lMidia4.setIdade("24");
-		lMidia4.setCpf("7363336433236");
-		lMidia4.setAtivo(true);
-		
-		MidiaModel lMidia5 = new MidiaModel(null, null, null, null, null);
-		lMidia5.setNome("Matheus");
-		lMidia5.setCodigo((long) 5);
-		lMidia5.setIdade("24");
-		lMidia5.setCpf("1264736433236");
-		lMidia5.setAtivo(true);
-		
-		midiaBO.inserir(lMidia1);
-		midiaBO.inserir(lMidia2);
-		midiaBO.inserir(lMidia3);
-		midiaBO.inserir(lMidia4);
-		midiaBO.inserir(lMidia5);
-		
+			
+		midiaBO.inserir(lMidia1);		
 	}
 	
 	public void excluirMidia() throws Exception{
-		MidiaModel lMidia = new MidiaModel(null, null, null, null, null);
+		MidiaModel lMidia = new MidiaModel(null, null, null, null, null, null, null);
 		lMidia.setCodigo(2L);
 		
 		midiaBO.excluir(lMidia);
@@ -73,7 +40,7 @@ public class MidiaTest {
 	
 	
 	public void editarMidia() throws Exception{
-		MidiaModel lMidia = new MidiaModel(null, null, null, null, null);
+		MidiaModel lMidia = new MidiaModel(null, null, null, null, null, null, null);
 		lMidia.setCodigo(2L);		
 		
 		MidiaModel lMidiaBusca = midiaBO.buscar(lMidia);
