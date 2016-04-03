78a05e5bb56ff89f3b11494f31a7e447a96f5475
Neyde Karen <neydekaren@infojr.com.br>
2016-04-03 14:03:18 -0300

    DataBase foi atualizada para comportar Midia Model

--- Stags/src/com/indracompany/stags/dao/util/DataBase.java
+++ Stags/src/com/indracompany/stags/dao/util/DataBase.java
@@ -4,39 +4,56 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.indracompany.stags.model.ClienteModel;
+import com.indracompany.stags.model.MidiaModel;
 
 public class DataBase {
 	static{
 		listaCliente = new ArrayList<ClienteModel>();
+		listaMidia = new ArrayList<MidiaModel>();
 	}
 		
 	private static long sequencialCliente;
+	private static long sequencialMidia;
 	private static List<ClienteModel> listaCliente;
-	
+	private static List<MidiaModel> listaMidia;	
 	
 	public static long getCodigoCliente(){
 		return ++sequencialCliente;
 	}
 
-
+	public static long getCodigoMidia() {
+		return  ++sequencialMidia;
+	}
+	
 	public static long getSequencialCliente() {
 		return sequencialCliente;
 	}
-
+	
+	public static long getSequencialMidia() {
+		return sequencialMidia;
+	}
 
 	public static void setSequencialCliente(long sequencialCliente) {
 		DataBase.sequencialCliente = sequencialCliente;
 	}
 
-
+	public static void setSequencialMidia(long sequencialMidia) {
+		DataBase.sequencialMidia = sequencialMidia;
+	}
+	
 	public static List<ClienteModel> getListaCliente() {
 		return listaCliente;
 	}
 
-
+	public static List<MidiaModel> getListaMidia() {
+		return listaMidia;
+	}
+	
 	public static void setListaCliente(List<ClienteModel> listaCliente) {
 		DataBase.listaCliente = listaCliente;
 	}
-	
 
+	public static void setListaMidia(List<MidiaModel> listaMidia) {
+		DataBase.listaMidia = listaMidia;
+	}
 }
