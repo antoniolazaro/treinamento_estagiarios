987613f8d37550b0251409189f39c072ddc8c949
Neyde Karen <neydekaren@infojr.com.br>
2016-04-03 13:49:00 -0300

    Interface dos métodos principais de mídia adicionada

---
+++ Stags/src/com/indracompany/stags/dao/MidiaDAOIf.java
@@ -0,0 +1,18 @@
+package com.indracompany.stags.dao;
+
+import java.util.List;
+
+import com.indracompany.stags.model.MidiaModel;
+
+public interface MidiaDAOIf {
+	
+	public void inserir(MidiaModel pModel) throws Exception;
+	
+	public void editar(MidiaModel pModel) throws Exception;
+	
+	public void excluir(MidiaModel pModel) throws Exception;
+	
+	public List<MidiaModel> listar() throws Exception;
+	
+	public MidiaModel buscar(MidiaModel pModel) throws Exception;
+}
